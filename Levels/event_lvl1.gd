d7c351743835238e5cb330aebaefdad28eab7bf0
Temulgeh <56922769+Temulgeh@users.noreply.github.com>
2021-10-03 11:20:41 +0200

    Add LE SCRIPT

--- Levels/event_lvl1.gd
+++ Levels/event_lvl1.gd
@@ -1,22 +1,22 @@
 extends Control
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var new_dialog = Dialogic.start('End_of_lvl1')
-	add_child(new_dialog)
-	$WindowDialog.openCmd()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if $WindowDialog.isopen:
-		$ViewportContainer/Viewport.gui_disable_input = true
-	else:
-		$ViewportContainer/Viewport.gui_disable_input = false
+#
+#
+## Declare member variables here. Examples:
+## var a = 2
+## var b = "text"
+#
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	var new_dialog = Dialogic.start('End_of_lvl1')
+#	add_child(new_dialog)
+#	$WindowDialog.openCmd()
+#	pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	if $WindowDialog.isopen:
+#		$ViewportContainer/Viewport.gui_disable_input = true
+#	else:
+#		$ViewportContainer/Viewport.gui_disable_input = false
