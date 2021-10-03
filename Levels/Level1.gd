b9ed4b6ef3f7de24e60aa7c755fcf8344d68dfc2
Tetane <stephane.roussel.fr@gmail.com>
2021-10-03 14:37:10 +0200

    add dialog to lvl

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -1,9 +1,12 @@
 extends ViewportContainer
 
+onready var dialog1 = load("res://addons/dialogic/Dialog.tscn")
+onready var diag1Instance = dialog1.instance()
 
 func _ready():
+	
 	PlayerTracker.player.connect("charged_shot", self, "first_interraction")
 
 
 func first_interraction():
-	pass
+	get_parent().get_node("Dialog").add_child(diag1Instance)
